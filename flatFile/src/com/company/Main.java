63efa3c52538435a33a781911ba57404c7fdec9b
Matthew Mecham <mmecham@idfbins.com>
2021-01-05 16:59:55 -0700

    accesses file name

--- flatFile/src/com/company/Main.java
+++ flatFile/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Flat File");
+        File file = new File("members.csv");
+        System.out.println("file name: " + file.getName());
+        System.out.println("file canRead: " + file.canRead());
+        System.out.println("file canWrite: " + file.canWrite());
+
     }
 }
