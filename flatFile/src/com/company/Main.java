f6a36f2825336e830dcf2bf898886dddd5116e84
Matthew Mecham <mmecham@idfbins.com>
2021-01-06 10:17:38 -0700

    file path established

--- flatFile/src/com/company/Main.java
+++ flatFile/src/com/company/Main.java
@@ -1,14 +1,34 @@
 package com.company;
+
 import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Flat File");
-        File file = new File("members.csv");
+
+        Path path = Paths.get("list.csv");
+        String fileName = "list.csv";
+        String location = path.toAbsolutePath().toString().replace(fileName, "\\src\\com\\company\\" + fileName);
+        File file = new File(location);
         System.out.println("file name: " + file.getName());
         System.out.println("file canRead: " + file.canRead());
         System.out.println("file canWrite: " + file.canWrite());
+        System.out.println("file exists: " + file.exists());
+        try {
+            Scanner scanner = new Scanner(file);
+            System.out.println(scanner.nextLine());
+            System.out.println(scanner.nextLine());
+            System.out.println(scanner.nextLine());
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
 
     }
 }
